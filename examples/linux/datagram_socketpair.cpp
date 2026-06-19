cabfacb1fd760bd78ed75adc6f5c2431dce04230
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2026-06-19 10:52:29 -0400

    Use on_readable in Linux datagram example

--- examples/linux/datagram_socketpair.cpp
+++ examples/linux/datagram_socketpair.cpp
@@ -1,28 +1,40 @@
 #include <iostream>
+#include <sys/socket.h>
+#include <unistd.h>
 
 #include "pypilot_event_loop.hpp"
+#include "pypilot_event_loop_linux/linux_fd_datagram_stream.hpp"
 
 int main() {
+    int fds[2];
+    if (socketpair(AF_UNIX, SOCK_DGRAM, 0, fds) != 0) {
+        return 1;
+    }
+
     pypilot_event_loop::EventLoop<> event_loop;
-    pypilot_event_loop::StaticDatagramStream<4, 32> stream;
+    pypilot_event_loop::LinuxFdDatagramStream receiver(fds[0]);
+    pypilot_event_loop::LinuxFdDatagramStream sender(fds[1]);
 
     int packets_read = 0;
 
-    event_loop.on_delay(0, [&stream]() {
-        const uint8_t msg[] = {'d', 'a', 't', 'a'};
-        stream.send(msg, sizeof(msg));
-    });
-
-    event_loop.on_repeat(1, [&event_loop, &stream, &packets_read]() {
+    event_loop.on_readable(receiver, [&]() {
         uint8_t buf[16];
-        const int n = stream.recv(buf, sizeof(buf));
+        const int n = receiver.recv(buf, sizeof(buf));
         if (n > 0) {
             packets_read++;
-            std::cout << "datagram stream read " << n << " bytes" << std::endl;
+            std::cout << "datagram readiness read " << n << " bytes" << std::endl;
             event_loop.request_exit();
         }
     });
 
+    event_loop.on_delay(0, [&]() {
+        const uint8_t msg[] = {'d', 'a', 't', 'a'};
+        sender.send(msg, sizeof(msg));
+    });
+
     event_loop.run_forever();
+
+    close(fds[0]);
+    close(fds[1]);
     return packets_read == 1 ? 0 : 1;
 }

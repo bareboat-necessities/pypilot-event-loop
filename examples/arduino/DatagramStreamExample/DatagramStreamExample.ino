b097048a03ef7dde05321c15f02fd7194cd21b4d
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2026-06-19 09:05:20 -0400

    Add Arduino datagram stream example

---
+++ examples/arduino/DatagramStreamExample/DatagramStreamExample.ino
@@ -0,0 +1,27 @@
+#include <Arduino.h>
+#include <pypilot_event_loop.hpp>
+
+pypilot_event_loop::EventLoop<> event_loop;
+pypilot_event_loop::StaticDatagramStream<4, 32> stream;
+uint32_t packets_read = 0;
+
+void setup() {
+    Serial.begin(115200);
+
+    event_loop.on_delay(0, []() {
+        const uint8_t msg[] = {'d', 'a', 't', 'a'};
+        stream.send(msg, sizeof(msg));
+    });
+
+    event_loop.on_repeat(1, []() {
+        uint8_t buf[16];
+        const int n = stream.recv(buf, sizeof(buf));
+        if (n > 0) {
+            packets_read++;
+        }
+    });
+}
+
+void loop() {
+    event_loop.tick();
+}

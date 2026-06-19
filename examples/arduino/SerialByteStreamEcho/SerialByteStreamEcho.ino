d9e5d9931578415642711bd5ea873bd842024db4
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2026-06-19 09:04:46 -0400

    Make Arduino byte stream example platform neutral

--- examples/arduino/SerialByteStreamEcho/SerialByteStreamEcho.ino
+++ examples/arduino/SerialByteStreamEcho/SerialByteStreamEcho.ino
@@ -1,18 +1,23 @@
 #include <Arduino.h>
 #include <pypilot_event_loop.hpp>
-#include <pypilot_event_loop_arduino/arduino_serial_stream.hpp>
 
 pypilot_event_loop::EventLoop<> event_loop;
-pypilot_event_loop::ArduinoSerialStream serial_stream(Serial);
+pypilot_event_loop::StaticByteStream<64> stream;
+uint32_t bytes_read = 0;
 
 void setup() {
     Serial.begin(115200);
 
-    event_loop.on_repeat(10, []() {
-        uint8_t buf[32];
-        const int n = serial_stream.read(buf, sizeof(buf));
+    event_loop.on_delay(0, []() {
+        const uint8_t msg[] = {'h', 'e', 'l', 'l', 'o'};
+        stream.write(msg, sizeof(msg));
+    });
+
+    event_loop.on_repeat(1, []() {
+        uint8_t buf[16];
+        const int n = stream.read(buf, sizeof(buf));
         if (n > 0) {
-            serial_stream.write(buf, static_cast<size_t>(n));
+            bytes_read += static_cast<uint32_t>(n);
         }
     });
 }

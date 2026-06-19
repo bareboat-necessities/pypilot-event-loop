a7850bf143b01bd03e4239d4d69b367cb6caaf18
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2026-06-19 17:21:53 -0400

    Keep protocol example stream local

--- examples/LineProtocolExample/LineProtocolExample.ino
+++ examples/LineProtocolExample/LineProtocolExample.ino
@@ -3,13 +3,51 @@
 #endif
 
 #include <pypilot_event_loop.hpp>
-#include "../support/example_byte_stream.hpp"
 
 using namespace pypilot_event_loop;
-using pypilot_event_loop_examples::ExampleByteStream;
+
+template<size_t Capacity>
+class LineInputStream final : public IByteStream {
+public:
+    int read(uint8_t* dst, size_t max_len) override {
+        if (!dst || max_len == 0) {
+            return 0;
+        }
+        size_t n = 0;
+        while (n < max_len && count_ > 0) {
+            dst[n++] = buffer_[head_];
+            head_ = (head_ + 1) % Capacity;
+            --count_;
+        }
+        return static_cast<int>(n);
+    }
+
+    int write(const uint8_t* src, size_t len) override {
+        if (!src || len == 0) {
+            return 0;
+        }
+        size_t n = 0;
+        while (n < len && count_ < Capacity) {
+            buffer_[tail_] = src[n++];
+            tail_ = (tail_ + 1) % Capacity;
+            ++count_;
+        }
+        return static_cast<int>(n);
+    }
+
+    bool readable() const override { return count_ > 0; }
+    bool writable() const override { return count_ < Capacity; }
+    bool valid() const override { return true; }
+
+private:
+    uint8_t buffer_[Capacity]{};
+    size_t head_ = 0;
+    size_t tail_ = 0;
+    size_t count_ = 0;
+};
 
 EventLoop<> event_loop;
-ExampleByteStream<256> stream;
+LineInputStream<256> stream;
 uint32_t line_count = 0;
 
 LineProtocolReader<128> lines(stream, LineProtocolOptions{}, [](LineView line) {

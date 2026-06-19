9e0451dea5c411a024b5454adcc36d2b97035b6f
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2026-06-19 17:42:25 -0400

    Select native TCP client and WiFi backend

--- src/pypilot_event_loop/native_tcp.hpp
+++ src/pypilot_event_loop/native_tcp.hpp
@@ -2,6 +2,11 @@
 
 #if defined(__linux__)
 #include "pypilot_event_loop_linux/linux_tcp_server.hpp"
+#include "pypilot_event_loop_linux/linux_tcp_client.hpp"
+#endif
+
+#if defined(ARDUINO) && defined(PYPILOT_EVENT_LOOP_ENABLE_ARDUINO_WIFI_TCP)
+#include "pypilot_event_loop_arduino/arduino_wifi_tcp.hpp"
 #endif
 
 namespace pypilot_event_loop {
@@ -9,6 +14,13 @@ namespace pypilot_event_loop {
 #if defined(__linux__)
 using NativeTcpServer = LinuxTcpServer;
 using NativeTcpConnection = LinuxTcpConnection;
+using NativeTcpClient = LinuxTcpClient;
+#endif
+
+#if defined(ARDUINO) && defined(PYPILOT_EVENT_LOOP_ENABLE_ARDUINO_WIFI_TCP)
+using NativeTcpServer = ArduinoWiFiTcpServer;
+using NativeTcpConnection = ArduinoWiFiTcpConnection;
+using NativeTcpClient = ArduinoWiFiTcpClient;
 #endif
 
 } // namespace pypilot_event_loop

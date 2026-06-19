2ea92e7487045b16d0bb7485e661bac12dde47cb
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2026-06-19 00:41:32 -0400

    Add timer tests

---
+++ tests/test_timer.cpp
@@ -0,0 +1,24 @@
+#include <cassert>
+#include "pypilot_event_loop.hpp"
+
+int main() {
+    using namespace pypilot_event_loop;
+
+    PeriodicTimer periodic(100);
+    assert(periodic.ready(0));
+    assert(!periodic.ready(50));
+    assert(periodic.ready(100));
+    assert(periodic.ready(350));
+    assert(!periodic.ready(399));
+    assert(periodic.ready(400));
+
+    OneShotTimer one;
+    one.arm(1000);
+    assert(!one.ready(999));
+    assert(one.ready(1000));
+    assert(!one.ready(1001));
+    assert(one.fired());
+    assert(!one.armed());
+
+    return 0;
+}

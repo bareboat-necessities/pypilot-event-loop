84e37f9e839cce4819793dc2cd9fd1ba64e5cc81
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2026-06-19 17:50:40 -0400

    Add UDP endpoint interface

---
+++ src/pypilot_event_loop/udp.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <stddef.h>
+#include <stdint.h>
+
+#include "pypilot_event_loop/datagram_stream.hpp"
+
+namespace pypilot_event_loop {
+
+struct UdpEndpoint {
+    char host[64]{};
+    uint16_t port = 0;
+};
+
+class IUdpDatagramStream : public IDatagramStream {
+public:
+    virtual bool bind(uint16_t local_port, bool reuse_address = true) = 0;
+    virtual bool set_remote(const char* host, uint16_t port) = 0;
+    virtual int send_to(const uint8_t* src, size_t len, const UdpEndpoint& endpoint) = 0;
+    virtual int recv_from(uint8_t* dst, size_t max_len, UdpEndpoint* source) = 0;
+};
+
+} // namespace pypilot_event_loop

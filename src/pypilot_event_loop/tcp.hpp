2620e777328cf4d4ef303c21a59f60818081852c
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2026-06-19 16:51:48 -0400

    Add common TCP listener interfaces

---
+++ src/pypilot_event_loop/tcp.hpp
@@ -0,0 +1,70 @@
+#pragma once
+
+#include <stddef.h>
+#include <stdint.h>
+
+namespace pypilot_event_loop {
+
+struct TcpListenOptions {
+    const char* host = "127.0.0.1";
+    uint16_t port = 0;
+    int backlog = -1;
+    bool reuse_address = true;
+    bool close_on_free = true;
+};
+
+struct TcpPeerInfo {
+    char host[64]{};
+    uint16_t port = 0;
+};
+
+class ITcpConnection {
+public:
+    virtual ~ITcpConnection() = default;
+
+    virtual bool valid() const = 0;
+    virtual void close() = 0;
+
+    virtual const TcpPeerInfo& peer() const = 0;
+    virtual size_t input_size() const = 0;
+    virtual size_t output_size() const = 0;
+
+    virtual int read(uint8_t* dst, size_t max_len) = 0;
+    virtual int write(const uint8_t* src, size_t len) = 0;
+
+    virtual bool read_exact(uint8_t* dst, size_t len) = 0;
+    virtual bool read_line(char* dst, size_t max_len, bool strip_cr = true) = 0;
+};
+
+class ITcpServerHandler {
+public:
+    virtual ~ITcpServerHandler() = default;
+
+    virtual void on_accept(ITcpConnection& connection, const TcpPeerInfo& peer) {
+        (void)connection;
+        (void)peer;
+    }
+
+    virtual void on_data(ITcpConnection& connection) {
+        (void)connection;
+    }
+
+    virtual void on_write_ready(ITcpConnection& connection) {
+        (void)connection;
+    }
+
+    virtual void on_close(ITcpConnection& connection) {
+        (void)connection;
+    }
+
+    virtual void on_error(ITcpConnection& connection, int error_code) {
+        (void)connection;
+        (void)error_code;
+    }
+
+    virtual void on_listener_error(int error_code) {
+        (void)error_code;
+    }
+};
+
+} // namespace pypilot_event_loop

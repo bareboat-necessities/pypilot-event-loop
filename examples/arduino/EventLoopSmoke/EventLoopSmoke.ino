53e181bd4abdf5e3db51c022222ad8ca9796185d
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2026-06-19 08:11:00 -0400

    Use callback facade in Arduino example

--- examples/arduino/EventLoopSmoke/EventLoopSmoke.ino
+++ examples/arduino/EventLoopSmoke/EventLoopSmoke.ino
@@ -1,26 +1,17 @@
 #include <Arduino.h>
 #include <pypilot_event_loop.hpp>
 
-class SmokeTask final : public pypilot_event_loop::IRuntimeTask {
-public:
-    void poll(uint64_t now_us) override {
-        last_us = now_us;
-        count++;
-    }
-
-    uint64_t last_us = 0;
-    uint32_t count = 0;
-};
-
-pypilot_event_loop::NativeClock event_clock;
-pypilot_event_loop::NativeScheduler event_scheduler(event_clock);
-SmokeTask smoke_task;
+pypilot_event_loop::EventLoop<> event_loop;
+uint32_t tick_count = 0;
 
 void setup() {
     Serial.begin(115200);
-    event_scheduler.add_periodic(smoke_task, 100000);
+
+    event_loop.onRepeat(100, []() {
+        tick_count++;
+    });
 }
 
 void loop() {
-    event_scheduler.run_once();
+    event_loop.tick();
 }

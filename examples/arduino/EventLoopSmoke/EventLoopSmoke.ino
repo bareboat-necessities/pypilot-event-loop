f28290c672c1b36a9bb15d9d2a9e71469b99735f
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2026-06-19 07:56:15 -0400

    Use native scheduler API in Arduino example

--- examples/arduino/EventLoopSmoke/EventLoopSmoke.ino
+++ examples/arduino/EventLoopSmoke/EventLoopSmoke.ino
@@ -1,12 +1,7 @@
 #include <Arduino.h>
 #include <pypilot_event_loop.hpp>
-#include <pypilot_event_loop_arduino/arduino_clock.hpp>
-#include <pypilot_event_loop_arduino/arduino_loop.hpp>
-#include <pypilot_event_loop_arduino/arduino_serial_stream.hpp>
 
-using namespace pypilot_event_loop;
-
-class SmokeTask final : public IRuntimeTask {
+class SmokeTask final : public pypilot_event_loop::IRuntimeTask {
 public:
     void poll(uint64_t now_us) override {
         last_us = now_us;
@@ -17,21 +12,15 @@ public:
     uint32_t count = 0;
 };
 
-ArduinoClock clock_;
-ArduinoLoop scheduler_(clock_);
-SmokeTask smoke_task_;
+pypilot_event_loop::NativeClock event_clock;
+pypilot_event_loop::NativeScheduler event_scheduler(event_clock);
+SmokeTask smoke_task;
 
 void setup() {
     Serial.begin(115200);
-    scheduler_.add_periodic(smoke_task_, 100000);
+    event_scheduler.add_periodic(smoke_task, 100000);
 }
 
 void loop() {
-    scheduler_.tick();
-
-    ArduinoSerialStream serial_stream(Serial);
-    const uint8_t msg[] = {'o', 'k', '\n'};
-    if (smoke_task_.count == 1) {
-        serial_stream.write(msg, sizeof(msg));
-    }
+    event_scheduler.run_once();
 }

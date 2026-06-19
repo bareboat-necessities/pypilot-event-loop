33b58b7b98400bab76fc599ab7f2783c572c85c9
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2026-06-19 00:38:49 -0400

    Add scheduler abstraction

---
+++ src/pypilot_event_loop/scheduler.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <stdint.h>
+#include "task.hpp"
+
+namespace pypilot_event_loop {
+
+class IScheduler {
+public:
+    virtual ~IScheduler() = default;
+
+    virtual bool add_periodic(IRuntimeTask& task, uint64_t period_us) = 0;
+    virtual bool add_one_shot(IRuntimeTask& task, uint64_t due_us) = 0;
+
+    virtual void run_once() = 0;
+    virtual void run_forever() = 0;
+    virtual void request_exit() = 0;
+};
+
+} // namespace pypilot_event_loop

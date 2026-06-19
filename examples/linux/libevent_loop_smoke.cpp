3faa23d74f8f0d1a7f284c11165864faea48adcf
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2026-06-19 07:51:48 -0400

    Use native event loop API in Linux example

--- examples/linux/libevent_loop_smoke.cpp
+++ examples/linux/libevent_loop_smoke.cpp
@@ -1,36 +1,35 @@
 #include <iostream>
 
-#include "pypilot_event_loop_linux/libevent_loop.hpp"
-#include "pypilot_event_loop_linux/monotonic_clock.hpp"
+#include "pypilot_event_loop.hpp"
 
 class PrintTask final : public pypilot_event_loop::IRuntimeTask {
 public:
-    explicit PrintTask(pypilot_event_loop::LinuxLibeventLoop& loop) : loop_(loop) {}
+    explicit PrintTask(pypilot_event_loop::NativeScheduler& scheduler) : scheduler_(scheduler) {}
 
     void poll(uint64_t now_us) override {
         std::cout << "tick " << count_ << " at " << now_us << " us" << std::endl;
         if (++count_ >= 3) {
-            loop_.request_exit();
+            scheduler_.request_exit();
         }
     }
 
 private:
-    pypilot_event_loop::LinuxLibeventLoop& loop_;
+    pypilot_event_loop::NativeScheduler& scheduler_;
     int count_ = 0;
 };
 
 int main() {
-    pypilot_event_loop::LinuxMonotonicClock clock;
-    pypilot_event_loop::LinuxLibeventLoop loop(clock);
-    if (!loop.valid()) {
+    pypilot_event_loop::NativeClock clock;
+    pypilot_event_loop::NativeScheduler scheduler(clock);
+    if (!scheduler.valid()) {
         return 1;
     }
 
-    PrintTask task(loop);
-    if (!loop.add_periodic(task, 100000)) {
+    PrintTask task(scheduler);
+    if (!scheduler.add_periodic(task, 100000)) {
         return 2;
     }
 
-    loop.run_forever();
+    scheduler.run_forever();
     return 0;
 }

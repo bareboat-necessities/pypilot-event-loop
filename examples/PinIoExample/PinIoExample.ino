75ac2d939c2d7b61c1d6fc93584d55e55f435be1
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2026-06-19 20:26:01 -0400

    Construct pin IO example pins during setup

--- examples/PinIoExample/PinIoExample.ino
+++ examples/PinIoExample/PinIoExample.ino
@@ -25,16 +25,13 @@ EventLoop<> event_loop;
 bool digital_out_level = false;
 int analog_out_value = 0;
 
+IDigitalInputPin* digital_in = nullptr;
+IDigitalOutputPin* digital_out = nullptr;
+IAnalogInputPin* analog_in = nullptr;
+IAnalogOutputPin* analog_out = nullptr;
+
 #if defined(ARDUINO)
-NativeDigitalInputPin digital_in(PYPILOT_DIGITAL_IN_PIN, DigitalPinMode::InputPullup);
-NativeDigitalOutputPin digital_out(PYPILOT_DIGITAL_OUT_PIN);
-NativeAnalogInputPin analog_in(PYPILOT_ANALOG_IN_PIN);
-NativeAnalogOutputPin analog_out(PYPILOT_ANALOG_OUT_PIN);
-#else
-NativeDigitalInputPin* digital_in = nullptr;
-NativeDigitalOutputPin* digital_out = nullptr;
-NativeAnalogInputPin* analog_in = nullptr;
-NativeAnalogOutputPin* analog_out = nullptr;
+bool setup_failed = false;
 #endif
 
 static void print_text(const char* text) {
@@ -53,15 +50,39 @@ static void print_number(int value) {
 #endif
 }
 
+#if defined(ARDUINO)
+static bool setup_pins() {
+    static NativeDigitalInputPin din(PYPILOT_DIGITAL_IN_PIN, DigitalPinMode::InputPullup);
+    static NativeDigitalOutputPin dout(PYPILOT_DIGITAL_OUT_PIN);
+    static NativeAnalogInputPin ain(PYPILOT_ANALOG_IN_PIN);
+    static NativeAnalogOutputPin aout(PYPILOT_ANALOG_OUT_PIN);
+    digital_in = &din;
+    digital_out = &dout;
+    analog_in = &ain;
+    analog_out = &aout;
+    return digital_in->valid() && digital_out->valid() && analog_in->valid() && analog_out->valid();
+}
+#else
+static bool setup_pins(const char* digital_in_path,
+                       const char* digital_out_path,
+                       const char* analog_in_path,
+                       const char* analog_out_path) {
+    static NativeDigitalInputPin din(digital_in_path);
+    static NativeDigitalOutputPin dout(digital_out_path);
+    static NativeAnalogInputPin ain(analog_in_path);
+    static NativeAnalogOutputPin aout(analog_out_path);
+    digital_in = &din;
+    digital_out = &dout;
+    analog_in = &ain;
+    analog_out = &aout;
+    return digital_in->valid() && digital_out->valid() && analog_in->valid() && analog_out->valid();
+}
+#endif
+
 static void setup_tasks() {
     event_loop.on_repeat(500, []() {
-#if defined(ARDUINO)
-        const bool d = digital_in.read();
-        const int a = analog_in.read();
-#else
         const bool d = digital_in && digital_in->read();
         const int a = analog_in ? analog_in->read() : 0;
-#endif
         print_text("digital in: ");
         print_text(d ? "1" : "0");
         print_text(" analog in: ");
@@ -71,11 +92,6 @@ static void setup_tasks() {
 
     event_loop.on_repeat(1000, []() {
         digital_out_level = !digital_out_level;
-#if defined(ARDUINO)
-        digital_out.write(digital_out_level);
-        analog_out.write(analog_out_value);
-        const int analog_max = analog_out.max_value();
-#else
         if (digital_out) {
             digital_out->write(digital_out_level);
         }
@@ -83,7 +99,6 @@ static void setup_tasks() {
             analog_out->write(analog_out_value);
         }
         const int analog_max = analog_out ? analog_out->max_value() : 255;
-#endif
         print_text("digital out: ");
         print_text(digital_out_level ? "1" : "0");
         print_text(" analog out: ");
@@ -99,10 +114,20 @@ static void setup_tasks() {
 #if defined(ARDUINO)
 void setup() {
     Serial.begin(115200);
+    if (!setup_pins()) {
+        print_text("pin io setup failed\n");
+        setup_failed = true;
+        return;
+    }
     setup_tasks();
 }
 
 void loop() {
+    if (setup_failed) {
+        print_text("pin io setup failed\n");
+        delay(1000);
+        return;
+    }
     event_loop.tick();
 }
 #else
@@ -111,14 +136,10 @@ int main(int argc, char** argv) {
         std::cerr << "usage: pin_io_example DIGITAL_IN_FILE DIGITAL_OUT_FILE ANALOG_IN_FILE ANALOG_OUT_FILE" << std::endl;
         return 2;
     }
-    NativeDigitalInputPin digital_in_file(argv[1]);
-    NativeDigitalOutputPin digital_out_file(argv[2]);
-    NativeAnalogInputPin analog_in_file(argv[3]);
-    NativeAnalogOutputPin analog_out_file(argv[4]);
-    digital_in = &digital_in_file;
-    digital_out = &digital_out_file;
-    analog_in = &analog_in_file;
-    analog_out = &analog_out_file;
+    if (!setup_pins(argv[1], argv[2], argv[3], argv[4])) {
+        std::cerr << "pin io setup failed" << std::endl;
+        return 1;
+    }
     setup_tasks();
     event_loop.run_forever();
     return 0;

6b8796ef747667a5252d22cabda14c46dffb1f57
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2026-06-19 20:00:39 -0400

    Add scheduler task removal API

--- src/pypilot_event_loop/scheduler.hpp
+++ src/pypilot_event_loop/scheduler.hpp
@@ -13,6 +13,7 @@ public:
 
     virtual bool add_periodic(IRuntimeTask& task, uint64_t period_us) = 0;
     virtual bool add_one_shot(IRuntimeTask& task, uint64_t due_us) = 0;
+    virtual bool remove(IRuntimeTask& task) = 0;
 
     virtual void run_once() = 0;
     virtual void run_forever() = 0;

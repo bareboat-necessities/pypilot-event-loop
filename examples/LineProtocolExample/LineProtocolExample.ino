dacfd9fd7f525ad3dd794942ce95d19d8967cc84
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2026-06-29 14:35:51 -0400

    Use memory stream in line protocol example

--- examples/LineProtocolExample/LineProtocolExample.ino
+++ examples/LineProtocolExample/LineProtocolExample.ino
@@ -6,48 +6,8 @@
 
 using namespace async_event_loop;
 
-template<size_t Capacity>
-class LineInputStream final : public IByteStream {
-public:
-    int read(uint8_t* dst, size_t max_len) override {
-        if (!dst || max_len == 0) {
-            return 0;
-        }
-        size_t n = 0;
-        while (n < max_len && count_ > 0) {
-            dst[n++] = buffer_[head_];
-            head_ = (head_ + 1) % Capacity;
-            --count_;
-        }
-        return static_cast<int>(n);
-    }
-
-    int write(const uint8_t* src, size_t len) override {
-        if (!src || len == 0) {
-            return 0;
-        }
-        size_t n = 0;
-        while (n < len && count_ < Capacity) {
-            buffer_[tail_] = src[n++];
-            tail_ = (tail_ + 1) % Capacity;
-            ++count_;
-        }
-        return static_cast<int>(n);
-    }
-
-    bool readable() const override { return count_ > 0; }
-    bool writable() const override { return count_ < Capacity; }
-    bool valid() const override { return true; }
-
-private:
-    uint8_t buffer_[Capacity]{};
-    size_t head_ = 0;
-    size_t tail_ = 0;
-    size_t count_ = 0;
-};
-
 EventLoop<> event_loop;
-LineInputStream<256> stream;
+MemoryByteStream<256> stream;
 uint32_t line_count = 0;
 
 LineProtocolReader<128> lines(stream, LineProtocolOptions{}, [](LineView line) {

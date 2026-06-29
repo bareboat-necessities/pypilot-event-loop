f38d77b9834603dc80c9a9e0ad752e526fd8a00c
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2026-06-29 12:54:53 -0400

    Make UDP datagrams use byte stream directly

--- src/async_event_loop/udp.hpp
+++ src/async_event_loop/udp.hpp
@@ -12,10 +12,18 @@ struct UdpEndpoint {
     uint16_t port = 0;
 };
 
-class IUdpDatagramStream : public IDatagramStream {
+class IUdpDatagramStream : public IByteStream {
 public:
+    bool is_datagram() const override { return true; }
+    bool writable() const override { return valid(); }
+
+    int read(uint8_t* dst, size_t max_len) override { return recv(dst, max_len); }
+    int write(const uint8_t* src, size_t len) override { return send(src, len); }
+
     virtual bool bind(uint16_t local_port, bool reuse_address = true) = 0;
     virtual bool set_remote(const char* host, uint16_t port) = 0;
+    virtual int send(const uint8_t* src, size_t len) = 0;
+    virtual int recv(uint8_t* dst, size_t max_len) = 0;
     virtual int send_to(const uint8_t* src, size_t len, const UdpEndpoint& endpoint) = 0;
     virtual int recv_from(uint8_t* dst, size_t max_len, UdpEndpoint* source) = 0;
 };
